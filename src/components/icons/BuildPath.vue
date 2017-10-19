d0113b647d0c1b6f3528bc92069912387d892bf0
Robert Carcassés Quevedo <carcasses@gmail.com>
2017-10-19 10:43:13 +0100

    Background image added. Controls added. Zoom in/out implemented.

---
+++ src/components/icons/BuildPath.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="32" height="32" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
+    <g transform="matrix(0.0625,0,0,0.0625,0,0)">
+      <path d="M426.7 341.3c-29.1 0-54.8 14.7-70.2 36.9L167.4 278.1c1.9-7.1 3.3-14.4 3.3-22.1 0-7.7-1.3-15-3.3-22.1l189.1-100.1c15.4 22.3 41 36.9 70.2 36.9 47.1 0 85.3-38.2 85.3-85.3S473.8 0 426.7 0s-85.3 38.2-85.3 85.3c0 .8.2 1.6.2 2.4l-199 105.4c-15.2-13.8-35.1-22.4-57.2-22.4C38.2 170.7 0 208.9 0 256s38.2 85.3 85.3 85.3c22.1 0 42-8.6 57.2-22.4l199 105.4c0 .8-.2 1.6-.2 2.4 0 47.1 38.2 85.3 85.3 85.3s85.3-38.2 85.3-85.3-38.1-85.4-85.2-85.4z" style="fill:rgb(43,36,92);fill-rule:nonzero;"/>
+    </g>
+  </svg>
+</template>
